58172af6d52661eb6f5d5a126b801dfd675be0b6
Ryan Frantz <ryanleefrantz@gmail.com>
2022-06-28 20:35:48 -0400

    Adds date and time picker.
    - Initializes the date and time picker with today's date and the nearest
      half hour, as a convenience.
    - Uses the selected date and time to define the timeline for the agenda.

--- js/budgenda.js
+++ js/budgenda.js
@@ -1,6 +1,32 @@
+/* Note to self: I'm focused on building agenda at the moment. Once a rough
+ * cut of that support is ready, I'll want to add support for adding notes
+ * to support tracking progress during a meeting. Notes will look similar to
+ * agenda items but their markup will indicate the difference.
+ */
+
 // Prefix a number with 0 if it's less than 10.
 function prefixZero(num) {
     return num < 10 ? `0${num}` : num;
+}
+
+function initializeDateAndTime() {
+  const now = new Date();
+  const agendaDate = document.querySelector("#agenda-date");
+  agendaDate.value = getYmd(now);
+  const agendaTime = document.querySelector("#agenda-time");
+  agendaTime.value = getNiceTime(now);
+}
+
+// Is there a better/more preferred way to handle this?
+window.addEventListener('load', initializeDateAndTime);
+
+/* Return an ISO8601 string of the date and time selected by the user for
+ * this agenda.
+ */
+function getSelectedDateAndTime() {
+  const agendaDate = document.querySelector("#agenda-date").value;
+  const agendaTime = document.querySelector("#agenda-time").value;
+  return `${agendaDate}T${agendaTime}`;
 }
 
 /*
@@ -15,10 +41,26 @@ function epoch(date) {
 
 // Return the time in HH:MM format.
 // If a Date argument is not present, finds the current date.
-function getTime(date = new Date()) {
-  let hour = prefixZero(date.getHours());
-  let min  = prefixZero(date.getMinutes());
+function getHhmm(date = new Date()) {
+  const hour = prefixZero(date.getHours());
+  const min  = prefixZero(date.getMinutes());
   return `${hour}:${min}`;
+}
+
+// Return a "nice" time. Nice is defined as the nearest half hour.
+function getNiceTime(date = new Date()) {
+  const increment = 1000 * 60 * 30; // 30-min increment
+  const nearestHalfHour = new Date(Math.round(date.getTime() / increment) * increment);
+  return getHhmm(nearestHalfHour);
+}
+
+// Return the date in YYYY-MM-DD format.
+// If a Date argument is not present, finds the current date.
+function getYmd(date = new Date()) {
+  const year  = prefixZero(date.getFullYear());
+  const month = prefixZero(date.getMonth() + 1);
+  const day   = prefixZero(date.getDate());
+  return `${year}-${month}-${day}`;
 }
 
 function addMinutes(numMinutes, date = new Date()) {
@@ -35,7 +77,7 @@ function createAgendaItem(event) {
 }
 
 function createAgenda() {
-  const now = new Date();
+  const agendeDateTime = new Date(getSelectedDateAndTime());
   const main = document.querySelector("main");
   const agenda = document.createElement("div");
   agenda.id = "agenda"
@@ -57,8 +99,8 @@ function createAgenda() {
     timeslot.addEventListener("click", createAgendaItem);
     // NOTE: We need to pass a copy of now, else now's value is updated,
     // by reference, creating an almost Fibonacci-like increase in time values!
-    const timeslotDatetime = addMinutes(i, new Date(now));
-    timeslot.setAttribute("data-time", getTime(timeslotDatetime));
+    const timeslotDatetime = addMinutes(i, new Date(agendeDateTime));
+    timeslot.setAttribute("data-time", getHhmm(timeslotDatetime));
     timeslot.id = `timeslot_${epoch(timeslotDatetime)}`;
     agenda.appendChild(timeslot);
   }
